dcba2df1e4d6e1a0667249b33402abd9fc95afcf
Mathews <mathewsshaji96@gmail.com>
2023-06-13 17:40:57 +0530

    test t1

--- scripts/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="/var/www/html/demoec2"
-if [ -d "$DIR" ]; then
-    echo "${DIR} exists"
-    sudo git -C "$DIR" pull
-else
-    sudo git clone https://github.com/DrUnkeN-SoUL/demoec2 "$DIR"
-fi
